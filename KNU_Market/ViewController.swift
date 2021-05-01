59ef671e7d5a588b04e58658ba8f681fed0d3bcb
김영채 <kevinkim2586@gmail.com>
2021-05-01 17:08:01 +0900

    Initialize project files

---
+++ KNU_Market/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KNU_Market
+//
+//  Created by Kevin Kim on 2021/05/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

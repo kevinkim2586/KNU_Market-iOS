510fad5567ccbda8302513410184590a5db6d9f3
김영채 <kevinkim2586@gmail.com>
2021-05-08 14:12:17 +0900

    feat: implement page control on item view controller

--- KNU_Market/Controllers/Item/ItemViewController.swift
+++ KNU_Market/Controllers/Item/ItemViewController.swift
@@ -4,15 +4,91 @@ class ItemViewController: UIViewController {
     
     @IBOutlet weak var itemImageView: UIImageView!
     @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var userProfileImageView: UIImageView!
+    @IBOutlet weak var userIdLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var itemTitleLabel: UILabel!
+    @IBOutlet weak var itemExplanationLabel: UILabel!
+    @IBOutlet weak var gatheringPeopleLabel: UILabel!
+    @IBOutlet weak var enterChatButton: UIButton!
+    
+    var images = ["bubble1", "bubble2", "bubble3"]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configurePageControl()
     
 
  
     }
     
+    @IBAction func pageChanged(_ sender: UIPageControl) {
+        
+        print("pageChange ACTIVATED")
+        
+        itemImageView.image = UIImage(named: images[pageControl.currentPage])
+    }
+    
+    
+}
 
+//MARK: - Page Control Configuration
+
+extension ItemViewController {
+    
+    func configurePageControl() {
+        
+        itemImageView.isUserInteractionEnabled = true
+        
+        pageControl.numberOfPages = images.count
+        pageControl.currentPage = 0
+        
+        pageControl.pageIndicatorTintColor = .lightGray
+        pageControl.currentPageIndicatorTintColor = .white
+        itemImageView.image = UIImage(named: images[0])
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self,
+                                                 action: #selector(self.respondToSwipeGesture(_:)))
+        swipeLeft.direction = UISwipeGestureRecognizer.Direction.left
+
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self,
+                                                  action: #selector(self.respondToSwipeGesture(_:)))
+        swipeRight.direction = UISwipeGestureRecognizer.Direction.right
+        
+        self.itemImageView.addGestureRecognizer(swipeLeft)
+        self.itemImageView.addGestureRecognizer(swipeRight)
+    }
+
+    @objc func respondToSwipeGesture(_ gesture: UIGestureRecognizer) {
+
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            
+
+            switch swipeGesture.direction {
+            
+            case UISwipeGestureRecognizer.Direction.left :
+                pageControl.currentPage += 1
+                itemImageView.image = UIImage(named: images[pageControl.currentPage])
+                
+            case UISwipeGestureRecognizer.Direction.right :
+                pageControl.currentPage -= 1
+                itemImageView.image = UIImage(named: images[pageControl.currentPage])
+            default:
+                break
+            }
+            
+        }
+
+    }
+    
+}
+
+//MARK: - UI Configuration
+
+extension ItemViewController {
+    
     
 }

4714522b2afc574bca834687b32ce534b561bcdd
Skyline-23 <bs2740@naver.com>
2021-11-15 21:54:30 +0900

    [DELETE] delete pod file

--- Pods/Target Support Files/HGPlaceholders/HGPlaceholders-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_HGPlaceholders : NSObject
-@end
-@implementation PodsDummy_HGPlaceholders
-@end
--- Pods/Target Support Files/RxAnimated/RxAnimated-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_RxAnimated : NSObject
-@end
-@implementation PodsDummy_RxAnimated
-@end

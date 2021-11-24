79fecded29548348c1b55dd32ee1b1261bc8a0dc
김영채 <kevinkim2586@gmail.com>
2021-11-24 10:18:54 +0900

    edit: remove comment

--- KNU_Market/Source/View Controllers/MyPage/VerifyOptionViewController.swift
+++ KNU_Market/Source/View Controllers/MyPage/VerifyOptionViewController.swift
@@ -129,15 +129,14 @@ class VerifyOptionViewController: BaseViewController {
     private func configure() {
         title = "학생 인증하기"
         setBackBarButtonItemTitle()
-        #warning("배포 할 때 아래 주석 해제")
-        //        if detectIfVerifiedUser() {
-        //            presentKMAlertOnMainThread(
-        //                title: "인증 회원 안내",
-        //                message: "이미 인증된 회원입니다.\n이제 공동구매를 즐겨보세요!",
-        //                buttonTitle: "확인"
-        //            )
-        //            navigationController?.popViewController(animated: true)
-        //        }
+        if detectIfVerifiedUser() {
+            presentKMAlertOnMainThread(
+                title: "인증 회원 안내",
+                message: "이미 인증된 회원입니다.\n이제 공동구매를 즐겨보세요!",
+                buttonTitle: "확인"
+            )
+            navigationController?.popViewController(animated: true)
+        }
     }
     
 }

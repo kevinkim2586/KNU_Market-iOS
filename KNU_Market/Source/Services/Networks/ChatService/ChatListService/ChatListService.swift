4b36ea79fee1fcb99b3f51888e4a14f8058f0229
김영채 <kevinkim2586@gmail.com>
2022-01-04 16:54:11 +0900

    feat: define ChatListService and Type

--- KNU_Market/Source/Services/Networks/ChatService/ChatListService/ChatListService.swift
+++ KNU_Market/Source/Services/Networks/ChatService/ChatListService/ChatListService.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import RxSwift
+
+final class ChatListService: ChatListServiceType {
+
+    fileprivate let network: Network<ChatAPI>
+    
+    init(network: Network<ChatAPI>) {
+        self.network = network
+    }
+    
+    func fetchJoinedChatList() -> Single<NetworkResultWithArray<Room>> {
+        
+        return network.requestArray(.getRoom, type: Room.self)
+            .map { result in
+                switch result {
+                case .success(let rooms):
+                    return .success(rooms)
+                case .error(let error):
+                    return .error(error)
+                }
+            }
+    }
+}
--- KNU_Market/Source/Services/Networks/ChatService/ChatListService/ChatListServiceType.swift
+++ KNU_Market/Source/Services/Networks/ChatService/ChatListService/ChatListServiceType.swift
@@ -8,7 +8,8 @@
 import Foundation
 import RxSwift
 
+//MARK: - 채팅 목록 불러오기 화면에서 쓰이는 메서드를 모아놓은 Protocol
+
 protocol ChatListServiceType: AnyObject {
-    
-    func fetchJoinedChatList()
+    func fetchJoinedChatList() -> Single<NetworkResultWithArray<Room>>
 }

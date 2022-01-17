8467513e47d89ed6cfeed2aff3b1950e01332e98
김영채 <kevinkim2586@gmail.com>
2022-01-17 15:37:29 +0900

    feat: implement BannerService and Type

---
+++ KNU_Market/Source/Model/Response Models/Banner Models/BannerModel.swift
@@ -0,0 +1,19 @@
+//
+//  BannerModel.swift
+//  KNU_Market
+//
+//  Created by Kevin Kim on 2022/01/17.
+//
+
+import Foundation
+
+struct BannerModel: ModelType {
+    
+    let bannerId: Int
+    let title: String
+    let media: MediaType
+    
+    enum CodingKeys: String, CodingKey {
+        case bannerId, title, media
+    }
+}

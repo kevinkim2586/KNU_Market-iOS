3642351b951f1a1d4169c61f0f6714d47aed6fa1
김영채 <kevinkim2586@gmail.com>
2022-01-23 15:22:25 +0900

    feat: add UITextField extension

--- KNU_Market/Source/Utilities + Extensions/Extensions/UITextField+Extension.swift
+++ KNU_Market/Source/Utilities + Extensions/Extensions/UITextField+Extension.swift
@@ -5,4 +5,51 @@
 //  Created by Kevin Kim on 2022/01/22.
 //
 
-import Foundation
+import UIKit
+
+extension UITextField {
+    
+    enum Direction {
+        case Left
+        case Right
+    }
+    
+    func addImage(direction: Direction, image: UIImage, colorSeparator: UIColor, colorBorder: UIColor){
+        let mainView = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 45))
+        mainView.layer.cornerRadius = 5
+        
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 45))
+        view.backgroundColor = .white
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 5
+        view.layer.borderWidth = CGFloat(0.5)
+        view.layer.borderColor = colorBorder.cgColor
+        mainView.addSubview(view)
+        
+        let imageView = UIImageView(image: image)
+        imageView.contentMode = .scaleAspectFit
+        imageView.frame = CGRect(x: 12.0, y: 10.0, width: 24.0, height: 24.0)
+        view.addSubview(imageView)
+        
+        let seperatorView = UIView()
+        seperatorView.backgroundColor = colorSeparator
+        mainView.addSubview(seperatorView)
+        
+        if(Direction.Left == direction){ // image left
+            seperatorView.frame = CGRect(x: 45, y: 0, width: 5, height: 45)
+            self.leftViewMode = .always
+            self.leftView = mainView
+        } else { // image right
+            seperatorView.frame = CGRect(x: 0, y: 0, width: 5, height: 45)
+            self.rightViewMode = .always
+            self.rightView = mainView
+        }
+        
+        self.layer.borderColor = colorBorder.cgColor
+        self.layer.borderWidth = CGFloat(0.5)
+        self.layer.cornerRadius = 5
+    }
+    
+}
+
+
